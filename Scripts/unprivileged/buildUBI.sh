68dee0131b6b8bee443e9706fd0d6d62f61dc02e
Irwin D'Souza <dsouzai.gh@gmail.com>
2022-09-27 10:20:05 -0400

    Add unprivileged ubi8 containerfiles and scripts
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

---
+++ Scripts/unprivileged/buildUBI.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+###############################################################################
+# Copyright 2022, IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+podman build -f Containerfiles/Containerfile.ubi8.unprivileged -t instantondemo:ubi8 . || exit 1
+podman build -f Containerfiles/Containerfile.ubi8.checkpoint.unprivileged -t instantoncheckpoint:ubi8 . || exit 1
+podman run --name checkpointrun --privileged -it instantoncheckpoint:ubi8 || exit 1
+podman wait checkpointrun || exit 1
+podman commit checkpointrun restorerun || exit 1
+podman rm checkpointrun || exit 1
---
+++ Scripts/unprivileged/restoreContainerUBI.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+###############################################################################
+# Copyright 2022, IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+podman run --rm --cap-add=CHECKPOINT_RESTORE --cap-add=SYS_PTRACE --cap-add=NET_ADMIN -v /proc/sys/kernel/ns_last_pid:/proc/sys/kernel/ns_last_pid --security-opt seccomp=criuseccompprofile.json --name restore_run restorerun
+
